c720ab7ad37888d9cf57e25040d6fe2322f341a9
Hunter Elbourn <hunterelbourn@gmail.com>
2017-07-13 22:17:58 -0700

    Adds tests.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
+let defaultList = require('./default_list.json');
+
 module.exports = function() {
-    let _phrases = [
-        "They will sing songs about us, they will climb to the mountain tops and say",
-        "The warrior stood above the fallen necromancer, sword drawn, and yelled",
-        "The trifling gnome stood before the beautiful princess and hissed",
-        "Brave, noble, and true. We are are heroes, but some might say we are",
-        "After claiming his reward, the knight looked down among the peasants and shouted"
-    ];
+    let _phrases = defaultList;
+    
+    console.getPhrases = function() {
+        return _phrases;
+    };
 
     console.tome = function(args) {
         if (!Array.isArray(args) || !args.length) {
@@ -15,11 +15,16 @@ module.exports = function() {
         }
 
         _phrases = args;
-    }
+    };
 
     console.lore = function(arg) {
         let phrase = _phrases[Math.floor(Math.random() * _phrases.length)];
         JSON.stringify()
         console.log(phrase + " '"  + JSON.stringify(arg) + "'.");
     };  
+
+    console.resetHistory = function() {
+        _phrases = defaultList;
+    };
+
 }();
--- test.js
+++ test.js
@@ -1,8 +1,27 @@
+'use strict';
+
+var assert = require('assert');
 require('./index.js');
 
-let badPhrase = { nope: "nope"} ;
-let goodPhrase = ["she wept"];
+describe('Lore Test', function() {
+    beforeEach(function() {
+        console.resetHistory();
+    });
 
-console.tome(badPhrase);
-console.tome(goodPhrase);
-console.lore("test");
+    it('is able to set different prefixes', function(done) {
+        let newPrefixList = ["hey there", "hi there", "hello there"];
+        console.tome(newPrefixList);
+        assert.equal(console.getPhrases(), newPrefixList);
+        done();
+    });
+
+    it('will reject a non-arrays for prefixes', function(done) {
+        let baseList = console.getPhrases();
+        let badPrefixList = {
+            didWeGetEmChat: "nope"
+        };
+        console.tome(badPrefixList);
+        assert.equal(console.getPhrases(), baseList);
+        done();
+    });
+});
